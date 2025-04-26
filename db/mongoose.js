bd404b136ac93e4de7a3444899b84da1b91a6800
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-04-26 17:36:51 +0530

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect("mongodb://localhost:27017/Storebook").then(()=>{
+mongoose.connect(process.env.DB_URL).then(()=>{
     console.log("db connected");
 });
 
